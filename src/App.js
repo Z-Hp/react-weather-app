abde8534bd26f41ea165a591f31b0a52c8001d4e
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-01-09 19:34:58 +0330

    Added Footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ function App() {
         Weather App
         <WeatherSearch />
       </header>
+      <footer>
+        <a
+          href="https://github.com/Z-Hp/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://zahrahosseinpour.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Zahra Hosseinpour
+        </a>
+      </footer>
     </div>
   );
 }
